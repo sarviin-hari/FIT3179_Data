c41869dcda407f36b28af34a217ff8187f1d7056
Sarviin a/l Hari <126446562+sarviin-hari@users.noreply.github.com>
2024-09-21 18:56:14 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "labour_population_map.json"; 
+vegaEmbed("#map_projection", vg_1).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+    }).catch(console.error);
